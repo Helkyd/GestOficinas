6da86c9cc37b6b8a79748f3e4f529cdd10898db5
helio <hcesar@gmail.com>
2016-10-27 12:08:00 +0100

    Import de dados para Avarias Import de dados para Marcas

--- gestoficinas/gestoficinas/doctype/avarias/avarias.py
+++ gestoficinas/gestoficinas/doctype/avarias/avarias.py
@@ -4,7 +4,12 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 from frappe.model.document import Document
+from frappe.model.naming import make_autoname
 
 class Avarias(Document):
-	pass
+
+	def autoname(self):
+		self.name = make_autoname(self.avarias + '-' + '.#####')
+	
